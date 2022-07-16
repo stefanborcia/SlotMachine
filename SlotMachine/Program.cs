1f005431ea55b26d76002488c45cbf7d41c3a5c7
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-07-16 17:50:49 +0200

    create if statements which cheks the middle line's

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -12,21 +12,70 @@ namespace SlotMachine
         {
 
             Console.WriteLine("Welcome to the Slot Machine !!! Succes !!!");
-
-            Random lineRandom =new Random();
-            int rows = 3;
-            int cols = 3;
-            int[,] arr = new int[rows, cols]; 
-            for( int i = 0; i < rows; i++)
+            Console.WriteLine("Spin by pressing the SpaceBar on Keyboard");
+            Console.WriteLine("Enter how much money you want to spend: $");
+            int number = 0;
+            bool succes = int.TryParse(Console.ReadLine(), out number);
+            if (!succes)
             {
-                for(int j = 0; j < cols; j++)
-                {
-                    arr[i, j] = lineRandom.Next(1, 8);
-                    Console.Write(arr[i, j] + " " );
-                }
-                Console.WriteLine(" ");
+                Console.WriteLine("Please enter a number ! ");
+                succes = true;
             }
-            
+            Console.WriteLine(" ");
+
+            Random lineRandom = new Random();
+            int cols = 3;
+            int rows = 3;
+            bool betting = true;
+            while (betting)
+            {
+                Console.WriteLine("-----");
+                int[,] grid = new int[cols, rows];
+                for (int i = 0; i < cols; i++)
+                {
+                    for (int j = 0; j < rows; j++)
+                    {
+                        grid[i, j] = lineRandom.Next(1, 8);
+                        Console.Write(grid[i, j] + " ");
+                    }
+                    Console.WriteLine(" ");
+                }
+                if (grid[0, 0] == grid[0, 1] && grid[0, 1] == grid[0, 2])
+                {
+                    Console.WriteLine($"Total win is: {number}$ ");
+                    number = number + 7;
+                }
+                if (grid[1, 0] == grid[1, 1] && grid[1, 1] == grid[1, 2])
+                {
+                    Console.WriteLine($"Total win is: {number}$ ");
+                    number = number + 10;
+                }
+                if (grid[2, 0] == grid[2, 1] && grid[2, 1] == grid[2, 2])
+                {
+                    Console.WriteLine($"Total win is: {number}$");
+                    number = number + 5;
+                }
+                Console.WriteLine("-----");
+                number--;
+                Console.WriteLine($"You have credit: {number}$ ");
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+                {
+                    betting = true;
+                }
+                else
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("You need to press the SpaceBar on keyboard to play");
+                    betting = true;
+                }
+                if (number <= 0)
+                {
+                    Console.WriteLine("You are out of money ");
+                    betting = false;
+                }
+            }
+
         }
     }
 }
